5d22636ff752bac5a44537f59e6ea54df5c265ad
pejro <pejro666@gmail.com>
2025-04-09 20:27:59 +0200

    Formated .tf file v3

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,16 +1,15 @@
 terraform {
   required_providers {
-    # Lifecycle management of AWS resources
     aws = {
       source  = "hashicorp/aws"
-        version = "5.94.1"
+      version = "5.94.1"
     }
   }
 
   backend "s3" {
-    bucket = "devops-static-web36"
-    key    = "state/terraform.tfstate"
-    region = "us-east-1"
+    bucket         = "devops-static-web36"
+    key            = "state/terraform.tfstate"
+    region         = "us-east-1"
   }
 
   required_version = "~> 1.11.2"
