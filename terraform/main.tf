c4270337521ff9d0cfce4b0ccdffea54b6075183
pejro <pejro666@gmail.com>
2025-04-10 18:07:05 +0200

    Rebuilt TF files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,38 +1,6 @@
-provider "aws" {
-  region = var.region
-}
-
-module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.31"
-
-  cluster_name                   = "devops-static-web"
-  cluster_version                = "1.31"
-  vpc_id                         = module.vpc.vpc_id
-  subnet_ids                     = module.vpc.private_subnets
-  cluster_endpoint_public_access = true
-
-  eks_managed_node_group_defaults = {
-    ami_type = "AL2_x86_64"
-  }
-
-  eks_managed_node_groups = {
-    one = {
-      name           = "node-group-1"
-      instance_types = ["t2.micro"]
-      min_size       = 1
-      max_size       = 3
-      desired_size   = 2
-    }
-
-    two = {
-      name           = "node-group-2"
-      instance_types = ["t2.micro"]
-      min_size       = 1
-      max_size       = 2
-      desired_size   = 1
-    }
-  }
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
 }
 
 data "aws_availability_zones" "available" {}
